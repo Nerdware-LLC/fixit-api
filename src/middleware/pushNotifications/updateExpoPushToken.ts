e084337ffd76f1be55806bff005949460d305771
trevor-anderson <T.AndersonProperty@gmail.com>
2022-10-05 10:25:37 -0400

    feat(PN-MW): convert 'updateExpoPushToken' to TS

--- src/middleware/pushNotifications/updateExpoPushToken.ts
+++ src/middleware/pushNotifications/updateExpoPushToken.ts
@@ -1,11 +1,13 @@
-import { User } from "@models/User";
+import { User, type UserType } from "@models/User";
 import { catchAsyncMW } from "@utils/middlewareWrappers";
 
 export const updateExpoPushToken = catchAsyncMW(async (req, res, next) => {
+  if (!req?._user) next("User not found");
+
   const {
     body: { expoPushToken },
     _user: { id: userID }
-  } = req;
+  } = req as { body: { expoPushToken?: string }; _user: UserType };
 
   if (expoPushToken) {
     await User.updateItem({ id: userID, sk: `#DATA#${userID}` }, { expoPushToken });
