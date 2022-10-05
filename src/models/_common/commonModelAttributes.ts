e44ea1c1f681d0743bca52329958bd89f5635423
trevor-anderson <T.AndersonProperty@gmail.com>
2022-10-05 13:53:36 -0400

    fix(models): ensure PHONE common attr doesnt pass undefined to transform fns

--- src/models/_common/commonModelAttributes.ts
+++ src/models/_common/commonModelAttributes.ts
@@ -6,10 +6,10 @@ export const COMMON_ATTRIBUTE_TYPES = {
   PHONE: {
     type: "string",
     transformValue: {
-      toDB: normalizeInput.phone, // Save only the digits
-      fromDB: prettifyStr.phone //   Convert "8881234567" to "(888) 123-4567"
+      toDB: (value?: string) => (value ? normalizeInput.phone(value) : value), // Save only the digits
+      fromDB: (value?: string) => (value ? prettifyStr.phone(value) : value) //   Convert "8881234567" to "(888) 123-4567"
     },
-    validate: (value: string) => !!value && /^\d{10,15}$/.test(value) // IRL global max phone length is 15
+    validate: (value?: string) => !!value && /^\d{10,15}$/.test(value) // IRL global max phone length is 15
   },
 
   DATETIME: {
