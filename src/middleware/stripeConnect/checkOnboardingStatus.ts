1784c0e615cbe8388f570fbdf42aeb06c2664332
trevor-anderson <T.AndersonProperty@gmail.com>
2022-10-08 10:55:39 -0400

    feat(MW-stripeConnect): convert from JW to TS, impl new Auth'd UserData type

---
+++ src/middleware/stripeConnect/checkOnboardingStatus.ts
@@ -0,0 +1,34 @@
+import { stripe } from "@lib/stripe";
+import { UserStripeConnectAccount } from "@models/UserStripeConnectAccount";
+import { catchAsyncMW, type APIRequestWithAuthenticatedUserData } from "@utils/middlewareWrappers";
+
+export const checkOnboardingStatus = catchAsyncMW<APIRequestWithAuthenticatedUserData>(
+  async (req, res, next) => {
+    const { id: userID, stripeConnectAccount } = req._user;
+
+    const { id, detailsSubmitted, chargesEnabled, payoutsEnabled } = stripeConnectAccount;
+
+    // prettier-ignore
+    const { details_submitted, charges_enabled, payouts_enabled } = await stripe.accounts.retrieve(id);
+
+    // Update DB if values are stale
+    if (
+      details_submitted !== !!detailsSubmitted ||
+      charges_enabled !== !!chargesEnabled ||
+      payouts_enabled !== !!payoutsEnabled
+    ) {
+      const updatedStripeConnectAccount = await UserStripeConnectAccount.updateOne(
+        { userID },
+        {
+          detailsSubmitted: !!details_submitted,
+          chargesEnabled: !!charges_enabled,
+          payoutsEnabled: !!payouts_enabled
+        }
+      );
+
+      req._user.stripeConnectAccount = updatedStripeConnectAccount;
+    }
+
+    next();
+  }
+);
