3d3d5556c7ada4762abd58306e768d1c70d7ba36
trevor-anderson <T.AndersonProperty@gmail.com>
2022-11-02 11:52:16 -0400

    feat(routes): update Stripe Link-mw to use /api base route

--- src/middleware/stripeConnect/createAccountLink.ts
+++ src/middleware/stripeConnect/createAccountLink.ts
@@ -2,18 +2,15 @@ import { stripe } from "@lib/stripe";
 import { ENV } from "@server/env";
 import { catchAsyncMW, type APIRequestWithAuthenticatedUserData } from "@utils/middlewareWrappers";
 
-const BASE_ONBOARDING_URL = `${ENV.CONFIG.SELF_URI}${ENV.STRIPE.CONNECT_ONBOARDING_REDIRECT_ROUTE}`;
-
-const RETURN_URL = `${BASE_ONBOARDING_URL}/return`;
-const REFRESH_URL = `${BASE_ONBOARDING_URL}/refresh`;
+const BASE_ONBOARDING_URL = `${ENV.CONFIG.SELF_URI}/connect`; // Goes to Fixit Web
 
 // req.originalUrl = "/connect/account-link"
 export const createAccountLink = catchAsyncMW<APIRequestWithAuthenticatedUserData>(
   async (req, res) => {
     const stripeLink = await stripe.accountLinks.create({
       account: req._user.stripeConnectAccount.id,
-      return_url: RETURN_URL,
-      refresh_url: REFRESH_URL,
+      return_url: `${BASE_ONBOARDING_URL}/return`,
+      refresh_url: `${BASE_ONBOARDING_URL}/refresh`,
       type: "account_onboarding"
     });
 
