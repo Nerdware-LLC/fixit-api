083a267f480be2ca131c98ec09ec70368051c0ce
trevor-anderson <T.AndersonProperty@gmail.com>
2022-12-01 07:28:12 -0500

    feat(apolloServer): add plugins and typing for apollo context

--- src/apolloServer.ts
+++ src/apolloServer.ts
@@ -1,27 +1,36 @@
-import { ApolloServer } from "@apollo/server";
+import { ApolloServer, type BaseContext } from "@apollo/server";
 import { schema } from "@graphql/schema";
 import { ENV } from "@server/env";
+import type { Request } from "express";
+import type { FixitApiAuthTokenPayload } from "./utils";
 
-const apolloServer = new ApolloServer({
+const apolloServer = new ApolloServer<ApolloServerResolverContext>({
   typeDefs: schema.typeDefs,
   resolvers: schema.resolvers,
   csrfPrevention: true,
-  ...(ENV.IS_PROD
-    ? { introspection: false }
-    : {
-        introspection: true,
-        plugins: [
-          await import("@apollo/server/plugin/landingPage/default").then(
-            ({ ApolloServerPluginLandingPageLocalDefault }) => {
-              return ApolloServerPluginLandingPageLocalDefault({ embed: true });
-            }
-          )
-        ],
-        ...(await import("@graphql/__tests__/utils/mocks")) // { mocks, mockEntireSchema }
-      })
+
+  introspection: !ENV.IS_PROD,
+  plugins: [
+    ...(ENV.NODE_ENV === "development"
+      ? // prettier-ignore
+        [
+          (await import("@apollo/server/plugin/schemaReporting")).ApolloServerPluginSchemaReporting(),
+          (await import("@apollo/server/plugin/landingPage/default")).ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+          (await import("@apollo/server/plugin/inlineTrace")).ApolloServerPluginInlineTrace()
+        ]
+      : [(await import("@apollo/server/plugin/disabled")).ApolloServerPluginLandingPageDisabled()])
+  ],
+  ...(ENV.NODE_ENV === "test" && (await import("@graphql/__tests__/utils/mocks"))) // { mocks, mockEntireSchema }
 });
 
 // Run required init logic for integrating with Express
 await apolloServer.start();
 
 export { apolloServer };
+
+/**
+ * The context available to all GQL resolvers.
+ */
+export interface ApolloServerResolverContext extends BaseContext, Partial<Request> {
+  user: FixitApiAuthTokenPayload;
+}
