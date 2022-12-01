919c586902eaeadc5e18d5002678a2366a3d0c03
trevor-anderson <T.AndersonProperty@gmail.com>
2022-12-01 07:34:57 -0500

    feat(mw/utils): add type-safety check on req.body

--- src/middleware/util-mw/logReqReceived.ts
+++ src/middleware/util-mw/logReqReceived.ts
@@ -5,7 +5,11 @@ export const logReqReceived = (req: Request, res: Response, next: NextFunction) 
   const { body, originalUrl } = req;
 
   // The operationName check ensures console-cluttering introspection queries aren't logged here
-  if (originalUrl === "/api" && Object.prototype.hasOwnProperty.call(body, "operationName")) {
+  if (
+    originalUrl === "/api" &&
+    body &&
+    Object.prototype.hasOwnProperty.call(body, "operationName")
+  ) {
     logger.gql(`request received (${new Date().toLocaleString()}) OPERATION ${body.operationName}`);
   } else {
     logger.server(`request received (${new Date().toLocaleString()}) PATH: ${originalUrl}`);
