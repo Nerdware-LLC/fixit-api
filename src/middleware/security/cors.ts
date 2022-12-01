b5e4f95b2acdc5f4545b7c243ed1b5a0eddc7d40
trevor-anderson <T.AndersonProperty@gmail.com>
2022-12-01 07:32:48 -0500

    feat(cors): add apollo+sentry related http headers and origins

--- src/middleware/security/cors.ts
+++ src/middleware/security/cors.ts
@@ -2,8 +2,23 @@ import cors from "cors";
 import { ENV } from "@server/env";
 
 const corsOptions = {
-  origin: [new RegExp(`^${ENV.CONFIG.SELF_URI}`), "https://studio.apollographql.com"],
-  allowedHeaders: ["Content-Type", "Authorization", "apollographql-client-name"]
+  origin: [
+    new RegExp(`^${ENV.CONFIG.API_BASE_URL}`),
+    "https://studio.apollographql.com",
+    ...(/^(dev|test)/.test(ENV.NODE_ENV) ? [/localhost/] : [])
+  ],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    // Sentry tracing http headers:
+    "sentry-trace",
+    "baggage",
+    // Apollo GraphQL http headers:
+    "apollographql-client-name",
+    "apollographql-client-version",
+    // Enable ApolloServerPluginInlineTrace in dev:
+    ...(ENV.NODE_ENV === "development" ? ["apollo-federation-include-trace"] : [])
+  ]
 };
 
 export const corsMW = cors(corsOptions);
--- src/middleware/security/setSecureHttpHeaders.ts
+++ src/middleware/security/setSecureHttpHeaders.ts
@@ -2,47 +2,146 @@ import helmet from "helmet";
 import { ENV } from "@server/env";
 import type { Request, Response, NextFunction } from "express";
 
+/**
+ * HELMET'S DEFAULT CSP DIRECTIVES:
+ *
+ * - default-src 'self';
+ * - base-uri 'self';
+ * - block-all-mixed-content;
+ * - font-src 'self' https: data:;
+ * - frame-ancestors 'self';
+ * - img-src 'self' data:;
+ * - object-src 'none';
+ * - script-src 'self';
+ * - script-src-attr 'none';
+ * - style-src 'self' https: 'unsafe-inline';
+ * - upgrade-insecure-requests
+ */
+const HELMET_DEFAULT_CSP_DIRECTIVES = helmet.contentSecurityPolicy.getDefaultDirectives() as Record<
+  string,
+  string[]
+>;
+
+/**
+ * STRIPE'S REQUIRED CSP DIRECTIVES:
+ *
+ * // TODO ADD the Stripe-required CSP directives to lambda-edge-secure-headers
+ *
+ * The directives listed below are required in order for fixit-web to use the stripeJS lib.
+ * https://stripe.com/docs/security/guide#content-security-policy
+ *
+ * - connect-src 'https://api.stripe.com';
+ * - frame-src 'https://js.stripe.com' 'https://hooks.stripe.com';
+ * - script-src 'https://js.stripe.com';
+ */
+const STRIPE_REQUIRED_CSP_DIRECTIVES = {
+  "connect-src": "https://api.stripe.com",
+  "frame-src": ["https://js.stripe.com", "https://hooks.stripe.com"],
+  "script-src": "https://js.stripe.com"
+};
+
+/**
+ * CSP VIOLATION REPORTING DIRECTIVES:
+ *
+ * - report-to 'fixit-security';
+ * - report-uri `${ENV.CONFIG.API_FULL_URL}/admin/csp-violation`;
+ *
+ * > "report-uri" has been deprecated, but as of Nov 2022 most browsers don't
+ *   yet support the new 'report-to' directive (still experimental in Chrome).
+ */
+const CSP_VIOLATION_REPORTING_DIRECTIVES = {
+  "report-uri": `${ENV.CONFIG.API_FULL_URL}/admin/csp-violation`,
+  "report-to": "fixit-security"
+};
+
+/**
+ * FIXIT API SRC CSP DIRECTIVES:
+ *
+ * - default-src `'${ENV.CONFIG.API_BASE_URL}'`;
+ * - script-src `'${ENV.CONFIG.API_BASE_URL}'`;
+ */
+const FIXIT_API_SRC_CSP_DIRECTIVES = {
+  "default-src": `'${ENV.CONFIG.API_BASE_URL}'`,
+  "script-src": `'${ENV.CONFIG.API_BASE_URL}'`
+};
+
+/**
+ * Security Middleware: `setSecureHttpHeaders`
+ *
+ * HELMET HTTP HEADERS (https://helmetjs.github.io/)
+ *
+ * The Helmet config results in the combined set of CSP directives listed below.
+ * These directives are a combination of helmet defaults, Stripe requirements,
+ * and custom Fixit-related sources.
+ *
+ * CSP DIRECTIVES:
+ *
+ * - default-src `'self' '${ENV.CONFIG.API_BASE_URL}';`
+ * - script-src `'self' '${ENV.CONFIG.API_BASE_URL}' 'https://js.stripe.com';`
+ * - script-src-attr `'none';`
+ * - frame-src `'https://js.stripe.com' 'https://hooks.stripe.com';`
+ * - frame-ancestors `'self';`
+ * - connect-src `'https://api.stripe.com';`
+ * - base-uri `'self';`
+ * - font-src `'self' https: data:;`
+ * - img-src `'self' data:;`
+ * - object-src `'none';`
+ * - style-src `'self' https: 'unsafe-inline';`
+ * - report-to `'fixit-security';`
+ * - report-uri `'${ENV.CONFIG.API_FULL_URL}/admin/csp-violation';`
+ * - `block-all-mixed-content;`
+ * - `upgrade-insecure-requests;`
+ */
+const helmetMW = helmet({
+  contentSecurityPolicy: {
+    directives: [
+      HELMET_DEFAULT_CSP_DIRECTIVES,
+      FIXIT_API_SRC_CSP_DIRECTIVES,
+      CSP_VIOLATION_REPORTING_DIRECTIVES,
+      STRIPE_REQUIRED_CSP_DIRECTIVES
+    ].reduce<Record<string, string[]>>((accum, cspDirectivesObject) => {
+      // Deep merge each set of CSP directives
+      Object.entries(cspDirectivesObject).forEach(([cspKey, cspValues]) => {
+        // Wrap single cspValue strings in an array
+        if (!Array.isArray(cspValues)) cspValues = [cspValues] as Array<string>;
+        // Add the cspKey if it hasn't already been added; merge in cspValues.
+        if (cspKey in accum) accum[cspKey].concat(cspValues);
+        else accum[cspKey] = cspValues;
+      });
+      return accum;
+    }, {})
+  }
+});
+
+/**
+ * `setSecureHttpHeaders` uses Helmet to set security-related HTTP headers.
+ *
+ * See `helmetMW` for a breakdown of the `Content-Security-Policy` directives.
+ *
+ * Aside from the Helmet-defaults, a list of which is available [here](https://helmetjs.github.io/),
+ * this middleware also sets the following headers:
+ *
+ * - Cache-Control: `no-store`
+ * - Report-To:
+ *    ```json
+ *    {
+ *      "group": "fixit-security",
+ *      "max_age": 10886400,
+ *      "url": "${ENV.CONFIG.API_FULL_URL}/admin/csp-violation"
+ *    }
+ *    ```
+ */
 export const setSecureHttpHeaders = (req: Request, res: Response, next: NextFunction) => {
   res.set({
     "Cache-Control": "no-store",
-    "Report-To": REPORT_TO
+    "Report-To": REPORT_TO_HTTP_HEADER_VALUE_JSON
   });
 
   helmetMW(req, res, next);
 };
 
-const CSP_VIOLATION_REPORTS_URI = `${ENV.CONFIG.API_FULL_URL}/admin/csp-violation`;
-
-const REPORT_TO = JSON.stringify({
+const REPORT_TO_HTTP_HEADER_VALUE_JSON = JSON.stringify({
   group: "fixit-security",
   max_age: 10886400,
-  url: CSP_VIOLATION_REPORTS_URI
+  url: CSP_VIOLATION_REPORTING_DIRECTIVES["report-uri"]
 });
-
-const TRUSTED_SOURCES = ["'self'", `'${ENV.CONFIG.SELF_URI}'`];
-
-const helmetMW = helmet({
-  contentSecurityPolicy: {
-    directives: {
-      ...helmet.contentSecurityPolicy.getDefaultDirectives(), // see below
-      "default-src": TRUSTED_SOURCES,
-      "report-uri": [CSP_VIOLATION_REPORTS_URI], // deprecated, but as of Feb 2021 most browsers don't yet support the new 'report-to' directive
-      "report-to": ["fixit-security"],
-      "script-src": TRUSTED_SOURCES
-    }
-  }
-});
-
-// HELMET'S DEFAULT CSP DIRECTIVES:
-//
-// default-src 'self';
-// base-uri 'self';
-// block-all-mixed-content;
-// font-src 'self' https: data:;
-// frame-ancestors 'self';
-// img-src 'self' data:;
-// object-src 'none';
-// script-src 'self';
-// script-src-attr 'none';
-// style-src 'self' https: 'unsafe-inline';
-// upgrade-insecure-requests
