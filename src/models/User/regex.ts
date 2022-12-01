7ba2b48c16c1f11d609b78ea2516865464591689
trevor-anderson <T.AndersonProperty@gmail.com>
2022-12-01 07:36:18 -0500

    feat(id-regex): update Stripe-ID regexs to reflect variable length

--- src/models/User/regex.ts
+++ src/models/User/regex.ts
@@ -3,4 +3,4 @@ import { UUID_V1_REGEX_STR } from "@utils/regex";
 export const USER_ID_REGEX_STR = `USER#${UUID_V1_REGEX_STR}`;
 export const USER_ID_REGEX = new RegExp(`^${USER_ID_REGEX_STR}$`);
 export const USER_SK_REGEX = new RegExp(`^#DATA#${USER_ID_REGEX_STR}$`);
-export const USER_STRIPE_CUSTOMER_ID_REGEX = /^cus_[a-zA-Z0-9]{14}$/; // Example from Stripe docs: "cus_IiU67YFNrznvI3"
+export const USER_STRIPE_CUSTOMER_ID_REGEX = /^cus_[a-zA-Z0-9]{10,}$/; // Example from Stripe docs: "cus_IiU67YFNrznvI3"
--- src/models/UserStripeConnectAccount/regex.ts
+++ src/models/UserStripeConnectAccount/regex.ts
@@ -4,4 +4,4 @@ export const STRIPE_CONNECT_ACCOUNT_SK_REGEX = new RegExp(
   `^STRIPE_CONNECT_ACCOUNT#${USER_ID_REGEX_STR}$`
 );
 
-export const STRIPE_CONNECT_ACCOUNT_STRIPE_ID_REGEX = /^acct_[a-zA-Z0-9]{16}$/;
+export const STRIPE_CONNECT_ACCOUNT_STRIPE_ID_REGEX = /^acct_[a-zA-Z0-9]{10,}$/;
