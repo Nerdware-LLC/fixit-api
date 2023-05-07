38ffa782b5e76f35bdb2ba1296be63ff7957f5ba
trevor-anderson <T.AndersonProperty@gmail.com>
2023-05-07 07:58:13 -0400

    refactor: split schema files into typeDefs+resolvers

---
+++ src/graphql/resolvers.ts
@@ -0,0 +1,74 @@
+import merge from "lodash.merge";
+import { logger } from "@utils/logger";
+import * as contact from "./Contact/resolvers";
+import * as fixitUser from "./FixitUser/resolvers";
+import * as invoice from "./Invoice/resolvers";
+import * as phoneContact from "./PhoneContact/resolvers";
+import * as profile from "./Profile/resolvers";
+import * as user from "./User/resolvers";
+import * as userSubscription from "./UserSubscription/resolvers";
+import * as workOrder from "./WorkOrder/resolvers";
+import * as dateTimeCustomScalar from "./_customScalars/DateTime/resolvers";
+import * as emailCustomScalar from "./_customScalars/Email/resolvers";
+
+/**
+ * Fixit API GQL Schema Resolvers
+ * - Each resolver is wrapped with a try/catch error-logger
+ */
+export const resolvers = Object.fromEntries(
+  Object.entries(
+    merge(
+      // CUSTOM SCALARS
+      dateTimeCustomScalar.resolvers,
+      emailCustomScalar.resolvers,
+      // INTERFACES
+      fixitUser.resolvers,
+      // CONCRETE TYPES
+      contact.resolvers,
+      invoice.resolvers,
+      profile.resolvers,
+      user.resolvers,
+      userSubscription.resolvers,
+      workOrder.resolvers,
+      // CLIENT-SPECIFIC
+      phoneContact.resolvers
+    )
+  ).map(([resolverType, resolversOfType]) => {
+    // resolverType is either "Query" or "Mutation"
+    return [
+      resolverType,
+      Object.fromEntries(
+        Object.entries(resolversOfType as Record<string, any>).map(([resolverName, resolver]) => {
+          // For scalar resolvers and whatnot, "resolver" won't be a function, just continue to next.
+          if (typeof resolver !== "function") return [resolverName, resolver];
+
+          // Resolver ID example: "QUERY:WorkOrder"
+          const resolverLogID = `${resolverType.toUpperCase()}:${resolverName}`;
+
+          /* Note: none of the sync resolver functions use `return new Promise...`, so this
+            logic using the resolver's constructor.name property works for this use case.  */
+          const wrappedResolverFn =
+            resolver.constructor.name === "AsyncFunction"
+              ? async (...args: any[]) => {
+                  return await resolver(...args).catch((err: unknown) => {
+                    // Log error and re-throw as-is
+                    logger.error(err, resolverLogID);
+                    throw err;
+                  });
+                }
+              : (...args: any[]) => {
+                  try {
+                    return resolver(...args);
+                  } catch (err: unknown) {
+                    // Log error and re-throw as-is
+                    logger.error(err, resolverLogID);
+                    throw err;
+                  }
+                };
+
+          return [resolverName, wrappedResolverFn];
+        })
+      ),
+    ];
+  })
+);
---
+++ src/graphql/typeDefs.ts
@@ -0,0 +1,47 @@
+import * as checklist from "./Checklist/typeDefs";
+import * as contact from "./Contact/typeDefs";
+import * as fixitUser from "./FixitUser/typeDefs";
+import * as invite from "./Invite/typeDefs";
+import * as invoice from "./Invoice/typeDefs";
+import * as location from "./Location/typeDefs";
+import * as phoneContact from "./PhoneContact/typeDefs";
+import * as profile from "./Profile/typeDefs";
+import * as user from "./User/typeDefs";
+import * as userStripeConnectAccount from "./UserStripeConnectAccount/typeDefs";
+import * as userSubscription from "./UserSubscription/typeDefs";
+import * as workOrder from "./WorkOrder/typeDefs";
+import * as deleteMutationResponse from "./_common/DeleteMutationResponse/typeDefs";
+import * as genericSuccessResponse from "./_common/GenericSuccessResponse/typeDefs";
+import * as dateTimeCustomScalar from "./_customScalars/DateTime/typeDefs";
+import * as emailCustomScalar from "./_customScalars/Email/typeDefs";
+import * as root from "./_root/typeDefs";
+
+/**
+ * Fixit API GQL Schema TypeDefs
+ */
+export const typeDefs = [
+  // EXTENDABLE ROOT TYPES
+  root.typeDefs,
+  // CUSTOM SCALARS
+  dateTimeCustomScalar.typeDefs,
+  emailCustomScalar.typeDefs,
+  // MUTATION REPONSE TYPES
+  deleteMutationResponse.typeDefs,
+  genericSuccessResponse.typeDefs,
+  // INTERFACES
+  fixitUser.typeDefs,
+  // CONCRETE TYPES
+  checklist.typeDefs,
+  contact.typeDefs,
+  invoice.typeDefs,
+  location.typeDefs,
+  profile.typeDefs,
+  userStripeConnectAccount.typeDefs,
+  user.typeDefs,
+  userSubscription.typeDefs,
+  workOrder.typeDefs,
+  // SIDE-EFFECT MUTATIONS
+  invite.typeDefs,
+  // CLIENT-SPECIFIC TYPES
+  phoneContact.typeDefs,
+];
