c2933c0493cc688cb80828ec405a8cc6d699593c
trevor-anderson <T.AndersonProperty@gmail.com>
2023-05-07 08:22:33 -0400

    style(prettier): implement new prettier rules

--- src/server/sentry.ts
+++ src/server/sentry.ts
@@ -1,9 +1,9 @@
 import * as Sentry from "@sentry/node";
-import "@sentry/tracing"; // <-- patches the global hub for tracing to work
+import "@sentry/tracing" /* <-- patches the global hub for tracing to work */;
 import { ENV } from "@server/env";
 
 Sentry.init({
   dsn: ENV.SENTRY_DSN,
   environment: ENV.NODE_ENV,
-  release: ENV.CONFIG.PROJECT_VERSION
+  release: ENV.CONFIG.PROJECT_VERSION,
 });
