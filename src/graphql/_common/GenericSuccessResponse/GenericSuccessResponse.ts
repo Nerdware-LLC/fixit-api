58de70e4f82139f152c816d96d63ae1b77b6d5fd
trevor-anderson <T.AndersonProperty@gmail.com>
2023-05-07 07:56:30 -0400

    feat: add GenericSuccessResponse gql response type

---
+++ src/graphql/_common/GenericSuccessResponse/GenericSuccessResponse.ts
@@ -0,0 +1,9 @@
+import type { GenericSuccessResponse as GenericSuccessResponseType } from "@types";
+
+export class GenericSuccessResponse implements GenericSuccessResponseType {
+  wasSuccessful: boolean;
+
+  constructor({ wasSuccessful }: { wasSuccessful: boolean }) {
+    this.wasSuccessful = wasSuccessful;
+  }
+}
