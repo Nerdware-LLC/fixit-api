c5d8b6ec5cf38b7f9dd6107136813a3b1fbb2637
trevor-anderson <T.AndersonProperty@gmail.com>
2023-06-28 13:28:07 -0400

    chore: update imports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,11 @@
 // util dirs:
 export * from "./customErrors";
-export * from "./logger";
 export * from "./prettifyStr";
 export * from "./typeSafety";
 // util files:
 export * from "./AuthToken";
 export * from "./jwt";
+export * from "./logger";
 export * from "./normalizeInput";
 export * from "./passwordHasher";
 export * from "./regex";
