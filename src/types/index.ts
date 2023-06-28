35c05432ba9ff6a8b006e8d8cc1e14c8f455e822
trevor-anderson <T.AndersonProperty@gmail.com>
2023-06-28 13:30:41 -0400

    feat: add modded types which fix/improve codegen types like the troublesome Maybe type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,6 @@
 // GQL Codegen Types
 export * from "./graphql";
-
-// Internal-facing Types (e.g., for database models)
-export * from "../models/Contact/types";
-export * from "../models/Invoice/types";
-export * from "../models/User/types";
-export * from "../models/UserStripeConnectAccount/types";
-export * from "../models/UserSubscription/types";
-export * from "../models/WorkOrder/types";
-
+// GQL Codegen Type Mods
+export * from "./graphqlTypeMods";
 // Other Types
 export * from "./express";
