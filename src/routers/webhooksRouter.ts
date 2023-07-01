6b0d6d475f9ef0098e75829956b6b448efd48950
trevor-anderson <T.AndersonProperty@gmail.com>
2023-07-01 09:45:53 -0400

    feat: add route '/stripe' to webhooksRouter

--- src/routers/webhooksRouter.ts
+++ src/routers/webhooksRouter.ts
@@ -8,13 +8,8 @@ import { handleStripeWebhookEvent } from "@middleware/stripeWebhooks";
  *
  * Descendant paths:
  * - `/api/webhooks/stripe`
- *
- * // FIXME paths were /api/webhooks/account and /api/webhooks/customer
- * //       NOW it's /api/webhooks/stripe
- * //       UPDATE the path in Stripe dashboard
  */
 export const webhooksRouter = express.Router();
 
-// For all Stripe-webhook requests, the Stripe webhook event is validated and logged.
-// TODO Once path is updated to /api/webhooks/stripe in Stripe dashboard, add path to this use-call
-webhooksRouter.use(handleStripeWebhookEvent);
+// For all Stripe-webhook requests, the Stripe webhook event is logged and validated.
+webhooksRouter.use("/stripe", handleStripeWebhookEvent);
