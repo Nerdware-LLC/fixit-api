e9697ed671d075c825314f67bd55e03d18d46102
trevor-anderson <T.AndersonProperty@gmail.com>
2023-07-16 14:42:16 -0400

    refactor: mv Location regex to models/Location

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -49,19 +49,3 @@ export const UNIX_TIMESTAMP_REGEX_STR = "\\d{10}";
  * Example v1 UUID: `2c5ea4c0-4067-11e9-8bad-9b1deb4d3b7d`
  */
 export const UUID_V1_REGEX_STR = "[a-z0-9]{8}(-[a-z0-9]{4}){3}-[a-z0-9]{12}";
-
-/**
- * Regex patterns for validating `Location` components.
- *
- * Location components are stored separately, despite some having patterns which are
- * currently the exact same and therefore duplicative. This is due to the fact that they
- * may differ when converted into unicode-based patterns to support i18n. Note that these
- * patterns all assume spaces have been replaced with underscores.
- */
-export const LOCATION_REGEX_STRS = {
-  COUNTRY: "[a-z-_]{2,}", //             Two or more letters/hyphens/underscores
-  REGION: "[a-z-_]{2,}", //              Two or more letters/hyphens/underscores
-  CITY: "[a-z-_]{2,}", //                Two or more letters/hyphens/underscores
-  STREET_LINE_1: "[a-z0-9-_.]{2,}", //   Two or more letters/hyphens/underscores/numbers/periods
-  STREET_LINE_2: "[a-z0-9-_.:#]{2,}", // Two or more letters/hyphens/underscores/numbers/periods/:/#
-};
