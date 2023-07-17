5172d2017bd788a6bf0806786787ab46047485a0
trevor-anderson <T.AndersonProperty@gmail.com>
2023-07-17 05:48:25 -0400

    fix: update AliasedItemPKs to ensure keys w defaults are optional

--- src/lib/dynamoDB/DdbSingleTable/types/keyTypes.ts
+++ src/lib/dynamoDB/DdbSingleTable/types/keyTypes.ts
@@ -1,15 +1,29 @@
 import type { ConditionalPick } from "type-fest";
 import type { TableKeysSchemaType, ModelSchemaType } from "./schemaTypes";
 
-export type ItemPrimaryKeys<Schema extends TableKeysSchemaType | ModelSchemaType> = {
-  -readonly [K in keyof GetKeyAttributes<Schema>]: string;
-};
-
-export type AliasedItemPrimaryKeys<Schema extends TableKeysSchemaType | ModelSchemaType> = {
-  -readonly [K in keyof GetKeyAttributes<Schema> as GetKeyAttributes<Schema>[K]["alias"] extends string
-    ? GetKeyAttributes<Schema>[K]["alias"]
-    : K]: string;
+export type AliasedItemPrimaryKeys<
+  Schema extends TableKeysSchemaType | ModelSchemaType,
+  KeyAttributesSchema extends GetKeyAttributes<Schema> = GetKeyAttributes<Schema>
+> = {
+  // This map will set RangeKey to optional if configured with a default
+  -readonly [K in keyof KeyAttributesSchema as KeyAttributesSchema[K] extends { isRangeKey: true }
+    ? KeyAttributesSchema[K]["default"] extends {}
+      ? GetAliasOrAttrName<KeyAttributesSchema, K>
+      : never
+    : never]+?: string;
+} & {
+  // Required - filter out RangeKey if configured with a default
+  -readonly [K in keyof KeyAttributesSchema as KeyAttributesSchema[K] extends { isRangeKey: true }
+    ? KeyAttributesSchema[K]["default"] extends {}
+      ? never
+      : GetAliasOrAttrName<KeyAttributesSchema, K>
+    : GetAliasOrAttrName<KeyAttributesSchema, K>]-?: string;
 };
 
 export type GetKeyAttributes<Schema extends TableKeysSchemaType | ModelSchemaType> =
   ConditionalPick<Schema, { isHashKey: true } | { isRangeKey: true }>;
+
+export type GetAliasOrAttrName<
+  Schema extends TableKeysSchemaType | ModelSchemaType,
+  AttrName extends keyof Schema
+> = Schema[AttrName]["alias"] extends string ? Schema[AttrName]["alias"] : AttrName;
