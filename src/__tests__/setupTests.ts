054e7684bf62944af2022f70d51391192427eda1
trevor-anderson <trevor@nerdware.cloud>
2023-07-18 12:18:42 -0400

    feat(tests): add vi.mock calls for aws-sdk pkgs

--- src/__tests__/setupTests.ts
+++ src/__tests__/setupTests.ts
@@ -2,4 +2,7 @@ import { vi } from "vitest";
 
 // Mock modules commonly used in many tests:
 
+vi.mock("@aws-sdk/client-dynamodb");
+vi.mock("@aws-sdk/client-lambda");
+vi.mock("@aws-sdk/lib-dynamodb");
 vi.mock("@server/env");
