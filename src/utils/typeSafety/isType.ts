1c929ad0a56840cc1bf5b9e1798b49b1ad5cfd17
trevor-anderson <trevor@nerdware.cloud>
2023-08-17 08:55:49 -0400

    feat: add types function,bigint,symbol,undefined,null

--- src/utils/typeSafety/isType.ts
+++ src/utils/typeSafety/isType.ts
@@ -1,12 +1,49 @@
+/** `string` type guard function */
+export const isString = (value: unknown): value is string => typeof value === "string";
+/** `number` type guard function */
+export const isNumber = (value: unknown): value is number => Number.isSafeInteger(value);
+/** `boolean` type guard function */
+export const isBoolean = (value: unknown): value is boolean => typeof value === "boolean";
+/** `function` type guard function */
+export const isFunction = (value: unknown): value is boolean => typeof value === "function";
+/** `BigInt` type guard function */
+export const isBigInt = (value: unknown): value is bigint => typeof value === "bigint";
+/** `Buffer` type guard function */
+export const isBuffer = (value: unknown): value is Buffer => Buffer.isBuffer(value);
+/** `Symbol` type guard function */
+export const isSymbol = (value: unknown): value is symbol => typeof value === "symbol";
+/** `undefined` type guard function */
+export const isUndefined = (value: unknown): value is undefined => typeof value === "undefined";
+/** `null` type guard function */
+export const isNull = (value: unknown): value is null => value === null;
+/** `Array` type guard function */
+export const isArray = (value: unknown): value is Array<unknown> => Array.isArray(value);
+/** `Date` type guard function */
+export const isDate = (value: unknown): value is Date => {
+  return value instanceof Date && !isNaN(value.getTime());
+};
+/** `Object` type guard fn which tests if `value` is a `Record<>` object (e.g. `{ foo: "bar" }`). */
+export const isRecordObject = <KeyTypes extends PropertyKey = string>(
+  value: unknown
+): value is Record<KeyTypes, unknown> => {
+  return typeof value === "object" && !Array.isArray(value) && value !== null;
+};
+
 /** Type guard functions */
 export const isType = {
-  string: (input: unknown): input is string => typeof input === "string",
-  number: (input: unknown): input is number => Number.isSafeInteger(input),
-  boolean: (input: unknown): input is boolean => typeof input === "boolean",
-  Buffer: (input: unknown): input is Buffer => Buffer.isBuffer(input),
-  Date: (input: unknown): input is Date => input instanceof Date && !isNaN(input.getTime()),
-  Array: (input: unknown): input is Array<unknown> => Array.isArray(input),
-  /** Test if `input` is a record/dictionary-like object (e.g. `{ foo: "bar" }`) */
-  Object: (input: unknown): input is Record<string, unknown> =>
-    typeof input === "object" && !Array.isArray(input) && input !== null,
+  string: isString,
+  number: isNumber,
+  boolean: isBoolean,
+  function: isFunction,
+  bigint: isBigInt,
+  Buffer: isBuffer,
+  symbol: isSymbol,
+  undefined: isUndefined,
+  null: isNull,
+  array: isArray,
+  Array: isArray,
+  object: isRecordObject,
+  Object: isRecordObject,
+  date: isDate,
+  Date: isDate,
 };
