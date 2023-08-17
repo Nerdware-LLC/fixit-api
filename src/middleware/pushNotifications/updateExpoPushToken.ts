38c919b3ddf39635a0f8eae317f9ba9134e26c58
trevor-anderson <trevor@nerdware.cloud>
2023-08-17 10:27:29 -0400

    docs: add jsdoc to updateExpoPushToken mw

--- src/middleware/pushNotifications/updateExpoPushToken.ts
+++ src/middleware/pushNotifications/updateExpoPushToken.ts
@@ -1,6 +1,12 @@
 import { mwAsyncCatchWrapper } from "@middleware/helpers";
 import { User } from "@models/User";
 
+/**
+ * On the Fixit mobile app, if the user's ExpoPushToken has changed/expired
+ * since the last login, the app will send the new one along with the request.
+ * This middleware checks the request body for `expoPushToken`, and updates
+ * the value in the database if one was provided.
+ */
 export const updateExpoPushToken = mwAsyncCatchWrapper<{ body: { expoPushToken?: string } }>(
   async (req, res, next) => {
     if (!req?._authenticatedUser) return next("User not found");
