70b9683756eaf2ce10bf65a6c4d453ec6a01ac4f
trevor-anderson <trevor@nerdware.cloud>
2023-08-17 09:02:10 -0400

    feat: update utils exports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,11 @@
 // util dirs:
-export * from "./customErrors";
+export * from "./clientInputHandlers";
 export * from "./prettifyStr";
 export * from "./typeSafety";
 // util files:
 export * from "./AuthToken";
-export * from "./jwt";
+export * from "./httpErrors";
 export * from "./logger";
-export * from "./normalizeInput";
 export * from "./passwordHasher";
 export * from "./regex";
 export * from "./uuid";
