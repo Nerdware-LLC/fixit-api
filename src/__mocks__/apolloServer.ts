b42967f3231beb00f0833aca5f8c4f2957a3faee
trevor-anderson <trevor@nerdware.cloud>
2023-08-17 08:40:20 -0400

    test(mocks): replace 'createApolloServer' with a mock apolloServer

--- src/__mocks__/apolloServer.ts
+++ src/__mocks__/apolloServer.ts
@@ -1,14 +1,30 @@
+import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginLandingPageDisabled } from "@apollo/server/plugin/disabled";
 import { addMocksToSchema } from "@graphql-tools/mock";
 import { fixitSchema } from "@graphql/schema";
-import { createApolloServer } from "@/createApolloServer";
+import type { ApolloServerResolverContext } from "@/apolloServer";
 
 /**
  * ### MOCK APOLLO SERVER
+ *
+ * For testing, `schema` is wrapped in `addMocksToSchema` from `@graphql-tools/mock`.
  *
  * Docs:
  * - https://www.apollographql.com/docs/apollo-server/testing/mocking
  * - https://www.apollographql.com/docs/apollo-server/testing/testing
  */
-export const apolloServer = await createApolloServer({
-  schema: addMocksToSchema({ schema: fixitSchema }),
+export const apolloServer = new ApolloServer<ApolloServerResolverContext>({
+  schema: addMocksToSchema({
+    schema: fixitSchema,
+    mocks: {
+      // TODO mocks go here
+    },
+    preserveResolvers: true,
+  }),
+  csrfPrevention: false,
+  introspection: false,
+  plugins: [ApolloServerPluginLandingPageDisabled()],
 });
+
+// Run required init logic for integrating with Express
+await apolloServer.start();
---
+++ src/apolloServer.ts
@@ -0,0 +1,26 @@
+import { ApolloServer, type BaseContext } from "@apollo/server";
+import { fixitSchema } from "@graphql/schema";
+import { ENV } from "@server/env";
+import type { FixitApiAuthTokenPayload } from "@utils/AuthToken";
+import type { Request } from "express";
+
+export const apolloServer = new ApolloServer<ApolloServerResolverContext>({
+  schema: fixitSchema,
+  csrfPrevention: true,
+  introspection: ENV.NODE_ENV === "development",
+  plugins: [
+    ...(ENV.NODE_ENV === "development"
+      ? [(await import("@apollo/server/plugin/inlineTrace")).ApolloServerPluginInlineTrace()]
+      : [(await import("@apollo/server/plugin/disabled")).ApolloServerPluginLandingPageDisabled()]),
+  ],
+});
+
+// Run required init logic for integrating with Express
+await apolloServer.start();
+
+/**
+ * The context available to all GQL resolvers.
+ */
+export interface ApolloServerResolverContext extends BaseContext, Partial<Request> {
+  user: FixitApiAuthTokenPayload;
+}
