122a14ac7495695d4eb27fe8941859cb50037834
trevor-anderson <trevor@nerdware.cloud>
2023-08-17 08:59:56 -0400

    docs: add jsdoc to getUnixTimestampUUID

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -1,3 +1,12 @@
 import { v1 as uuidv1 } from "uuid";
 
-export const getUnixTimestampUUID = () => uuidv1({ msecs: new Date().getTime() });
+/**
+ * Generates a unique UUID string with a timestamp based on the current or
+ * provided date.
+ *
+ * @param date - Optional. A `Date` timestamp for the UUID (default: `new Date()`).
+ * @returns A unique UUID string with a timestamp based on the provided or current date.
+ */
+export const getUnixTimestampUUID = (date: Date = new Date()) => {
+  return uuidv1({ msecs: date.getTime() });
+};
