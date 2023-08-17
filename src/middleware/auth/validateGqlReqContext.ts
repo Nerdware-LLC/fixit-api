37e79398d2124bcc63b028859fb6f2f2f1be8863
trevor-anderson <trevor@nerdware.cloud>
2023-08-17 09:13:59 -0400

    fix: update import path for http-errors

--- src/middleware/auth/validateGqlReqContext.ts
+++ src/middleware/auth/validateGqlReqContext.ts
@@ -1,7 +1,7 @@
 import { UserSubscription } from "@models/UserSubscription";
 import { ENV } from "@server/env";
 import { AuthToken } from "@utils/AuthToken";
-import { GqlAuthError, GqlPaymentRequiredError } from "@utils/customErrors";
+import { GqlAuthError, GqlPaymentRequiredError } from "@utils/httpErrors";
 import type { Request } from "express";
 import type { ApolloServerResolverContext } from "../../createApolloServer";
 
