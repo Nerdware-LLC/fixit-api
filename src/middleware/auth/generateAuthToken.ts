2753e67461872d9d7f171c53f075fcbc68158aae
trevor-anderson <trevor@nerdware.cloud>
2023-08-17 09:11:31 -0400

    docs: add jsdoc to generateAuthToken

--- src/middleware/auth/generateAuthToken.ts
+++ src/middleware/auth/generateAuthToken.ts
@@ -2,6 +2,13 @@ import { mwCatchWrapper } from "@middleware/helpers";
 import { AuthToken } from "@utils";
 import type { FixitApiAuthTokenPayloadUserData } from "@utils";
 
+/**
+ * This middleware generates an AuthToken for the authenticated User and
+ * returns it as a response. If the User is not found or the User's Stripe
+ * Connect account is not found, an error message is passed to `next`. If
+ * there are any pre-fetched user items, they are included in the response
+ * as "userItems".
+ */
 export const generateAuthToken = mwCatchWrapper((req, res, next) => {
   if (!req?._authenticatedUser) return next("User not found");
   if (!req._authenticatedUser?.stripeConnectAccount)
