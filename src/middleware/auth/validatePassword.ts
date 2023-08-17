6b9fc6337bd7ca95376d55eafc26aca8733588d8
trevor-anderson <trevor@nerdware.cloud>
2023-08-17 09:18:36 -0400

    docs: add jsdoc to validatePassword mw

--- src/middleware/auth/validatePassword.ts
+++ src/middleware/auth/validatePassword.ts
@@ -1,6 +1,11 @@
 import { mwAsyncCatchWrapper } from "@middleware/helpers";
 import { passwordHasher, AuthError } from "@utils";
 
+/**
+ * This middleware checks if the authenticated User's login type is "LOCAL",
+ * and if so, compares the provided password against the passwordHash stored
+ * in the db. If it's invalid, an AuthError is thrown.
+ */
 export const validatePassword = mwAsyncCatchWrapper<{ body: { password: string } }>(
   async (req, res, next) => {
     if (!req?._user) return next("User not found");
