ab4ff8d979bcf22d487ce8cc0b1f7532a392d43e
trevor-anderson <trevor@nerdware.cloud>
2023-08-17 09:10:44 -0400

    docs: add jsdoc to findUserByEmail mw

--- src/middleware/auth/findUserByEmail.ts
+++ src/middleware/auth/findUserByEmail.ts
@@ -1,6 +1,9 @@
 import { mwAsyncCatchWrapper } from "@middleware/helpers";
 import { User } from "@models/User";
 
+/**
+ * This middleware simply queries the DB for a User with the given email address.
+ */
 export const findUserByEmail = mwAsyncCatchWrapper<{ body: { email: string } }>(
   async (req, res, next) => {
     const [user] = await User.query({
