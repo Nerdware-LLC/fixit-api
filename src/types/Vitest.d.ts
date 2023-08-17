0a3e456665eb38e07ee26a59c9a62dcc822af203
trevor-anderson <trevor@nerdware.cloud>
2023-08-17 09:08:26 -0400

    test: add vitest type decs for custom matchers

---
+++ src/types/Vitest.d.ts
@@ -0,0 +1,31 @@
+/* eslint-disable */
+import type { Assertion, AsymmetricMatchersContaining } from "vitest";
+
+/*
+  This file adds custom matchers to the Vitest assertion interface.
+  See docs: https://vitest.dev/guide/extending-matchers.html
+*/
+
+interface CustomMatchers<T = any, R = unknown> {
+  /** Test if the `received` array only contains elements specified in the `expected` array. */
+  toOnlyContain(expected: Record<PropertyKey, unknown>): R;
+  /** Test if the `received` value matches one of the values in the `expected` array. */
+  toBeOneOf(expected: Array<unknown>): R;
+  /** Test if the `received` value matches one of the match-objects in the `expected` array. */
+  toMatchOneOf(expected: Array<Record<PropertyKey, unknown>>): R;
+  /** Test if the `received` value is a valid Date object OR ISO 8601 date string. */
+  toBeValidDate(): R;
+}
+
+declare module "vitest" {
+  interface Assertion<T = any, R = unknown> extends CustomMatchers<T, R> {}
+  interface AsymmetricMatchersContaining extends CustomMatchers {
+    /**
+     * Test if the `received` value passes the provided function.
+     * This is an asymmetric version of the existing [`toSatisfy`][toSatisfyLink] matcher.
+     *
+     * [toSatisfyLink]: https://vitest.dev/api/expect.html#tosatisfy
+     */
+    toSatisfyFn(matcherFn: (value: any) => boolean): any;
+  }
+}
