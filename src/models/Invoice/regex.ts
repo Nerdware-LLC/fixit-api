dcbf77b02403ba6afee39ac7aac35921a1a03e6b
trevor-anderson <trevor@nerdware.cloud>
2023-08-18 09:24:35 -0400

    fix: replace unix-timestamp regex w UUID regex

--- src/models/Invoice/regex.ts
+++ src/models/Invoice/regex.ts
@@ -1,6 +1,6 @@
 import { USER_ID_REGEX_STR } from "@models/User/regex";
-import { UNIX_TIMESTAMP_REGEX_STR } from "@utils/regex";
+import { UUID_V1_REGEX_STR } from "@utils/regex";
 
 export const INVOICE_SK_PREFIX_STR = "INV";
-export const INVOICE_SK_REGEX_STR = `^${INVOICE_SK_PREFIX_STR}#${USER_ID_REGEX_STR}#${UNIX_TIMESTAMP_REGEX_STR}$`;
+export const INVOICE_SK_REGEX_STR = `^${INVOICE_SK_PREFIX_STR}#${USER_ID_REGEX_STR}#${UUID_V1_REGEX_STR}$`;
 export const INVOICE_SK_REGEX = new RegExp(INVOICE_SK_REGEX_STR);
