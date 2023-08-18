39352c5ac4ef5efae01a25bcc62f04b1016c51a5
trevor-anderson <trevor@nerdware.cloud>
2023-08-18 10:25:51 -0400

    refactor: mv Stripe regex to isValidStripeID util

--- src/models/UserStripeConnectAccount/regex.ts
+++ src/models/UserStripeConnectAccount/regex.ts
@@ -3,9 +3,3 @@ import { USER_ID_REGEX_STR } from "@models/User/regex";
 export const STRIPE_CONNECT_ACCOUNT_SK_PREFIX_STR = "STRIPE_CONNECT_ACCOUNT";
 export const STRIPE_CONNECT_ACCOUNT_SK_REGEX_STR = `^${STRIPE_CONNECT_ACCOUNT_SK_PREFIX_STR}#${USER_ID_REGEX_STR}$`;
 export const STRIPE_CONNECT_ACCOUNT_SK_REGEX = new RegExp(STRIPE_CONNECT_ACCOUNT_SK_REGEX_STR);
-
-/**
- * Stripe connect account id regex
- * > Example id: `"acct_1GpaAGC34C0mN67J"`
- */
-export const STRIPE_CONNECT_ACCOUNT_STRIPE_ID_REGEX = /^acct_[a-zA-Z0-9]{10,}$/;
