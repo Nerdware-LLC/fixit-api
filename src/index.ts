e82fe74baa0505d866cd3420bbf98fa90b73fa46
trevor-anderson <trevor@nerdware.cloud>
2023-10-09 10:18:48 -0400

    fix: change utils import to logger file not index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "@/server/init";
 import { ENV } from "@/server/env";
-import { logger } from "@/utils";
+import { logger } from "@/utils/logger";
 import { expressApp } from "./expressApp";
 
 const server = expressApp.listen(ENV.CONFIG.PORT, () => {
