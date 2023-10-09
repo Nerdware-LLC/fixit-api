dd5c0db235cfe9726a5adc23905067f95a37021a
trevor-anderson <trevor@nerdware.cloud>
2023-10-09 12:19:45 -0400

    type: rm mock-finding logic from aws-sdk mocks

--- __mocks__/@aws-sdk/client-dynamodb.ts
+++ __mocks__/@aws-sdk/client-dynamodb.ts
@@ -1,45 +1,12 @@
 import { mockClient } from "aws-sdk-client-mock";
-import { MOCK_TABLE } from "@/tests/staticMockItems/ddbTable";
-import type {
-  ListTablesOutput,
-  DescribeTableInput,
-  DescribeTableOutput,
-  CreateTableInput,
-  CreateTableOutput,
-} from "@aws-sdk/client-dynamodb";
 
 const {
-  DynamoDBClient: _DynamoDBClient,
+  DynamoDBClient: Actual_DynamoDBClient,
   DescribeTableCommand,
   CreateTableCommand,
   ListTablesCommand,
 } = await vi.importActual<typeof import("@aws-sdk/client-dynamodb")>("@aws-sdk/client-dynamodb");
 
-const DynamoDBClient = vi.fn(() =>
-  mockClient(_DynamoDBClient)
-    .on(DescribeTableCommand)
-    .callsFake(({ TableName }: DescribeTableInput): DescribeTableOutput => {
-      return {
-        Table: {
-          ...MOCK_TABLE,
-          TableName,
-          TableStatus: "ACTIVE",
-          BillingModeSummary: { BillingMode: MOCK_TABLE.BillingMode },
-        },
-      };
-    })
-    .on(CreateTableCommand)
-    .callsFake(({ BillingMode, ...args }: CreateTableInput): CreateTableOutput => {
-      return {
-        TableDescription: {
-          ...args,
-          TableStatus: "ACTIVE",
-          BillingModeSummary: { BillingMode },
-        },
-      };
-    })
-    .on(ListTablesCommand)
-    .callsFake((): ListTablesOutput => ({ TableNames: [MOCK_TABLE.TableName] }))
-);
+const DynamoDBClient = vi.fn(() => mockClient(Actual_DynamoDBClient));
 
 export { DynamoDBClient, DescribeTableCommand, CreateTableCommand, ListTablesCommand };
