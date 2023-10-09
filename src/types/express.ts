2ca5cced2fd921a18afebc64b7bce2f6bf88c6b0
trevor-anderson <trevor@nerdware.cloud>
2023-10-09 12:01:19 -0400

    docs: expand jsdoc for PreFetchedUserQueryItems

--- src/types/express.ts
+++ src/types/express.ts
@@ -5,9 +5,17 @@ import type { FixitApiAuthTokenPayload } from "@/utils/AuthToken";
 import type { OverrideProperties, SetOptional } from "type-fest";
 
 /**
- * A User's pre-fetched WorkOrders, Invoices, and Contacts (used on logins).
- * When fetched, these objects are made available on Express Request objects
- * as `req._userQueryItems`.
+ * A User's pre-fetched WorkOrders, Invoices, and Contacts (used on logins). When fetched
+ * by the `queryUserItems` middleware (see `src/middleware/auth/queryUserItems.ts`), these
+ * objects are made available on Express Request objects as `req._userQueryItems`.
+ *
+ * Note: the middleware converts workOrders' and invoices' internal createdByUserID and
+ * assignedToUserID fields into createdBy and assignedTo objects to match the GQL schema,
+ * but only the "id" field can be provided on the createdBy/assignedTo objects without
+ * fetching additional data on the associated users/contacts from either the db or usersCache.
+ * The middleware forgoes fetching the data since the client-side Apollo cache already handles
+ * fetching additional data as needed (_if_ it's needed), and fetching it there can delay auth
+ * request response times, especially if the authenticating user has a large number of items.
  */
 export type PreFetchedUserQueryItems = {
   workOrders?: Array<
