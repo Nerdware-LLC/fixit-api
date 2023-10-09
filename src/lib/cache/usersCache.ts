cd39ae60e29abd2e548c5100a6fc9c5cc90bbf13
trevor-anderson <trevor@nerdware.cloud>
2023-10-09 12:21:14 -0400

    fix: correct typing to reflect User props

--- src/lib/cache/usersCache.ts
+++ src/lib/cache/usersCache.ts
@@ -1,7 +1,10 @@
 import { Cache } from "./Cache";
-import type { Contact } from "@/types";
+import type { User, Contact } from "@/types";
+import type { Simplify } from "type-fest";
 
 /**
  * Fixit API local cache instance for searching Users by `User.handle`.
+ *
+ * > Only public "Contact" fields are stored.
  */
-export const usersCache = new Cache<Contact>();
+export const usersCache = new Cache<Simplify<Pick<User, keyof Contact>>>();
