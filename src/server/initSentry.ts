1b19787f25e5d8eda1926accb82b58af31170f4b
trevor-anderson <trevor@nerdware.cloud>
2023-10-09 12:02:55 -0400

    refactor: impl option chain on read PROJECT_VERSION

--- src/server/initSentry.ts
+++ src/server/initSentry.ts
@@ -5,6 +5,6 @@ Sentry.init({
   enabled: /^(dev|staging|prod)/i.test(ENV.NODE_ENV) && !!ENV?.SENTRY_DSN,
   dsn: ENV?.SENTRY_DSN,
   environment: ENV.NODE_ENV,
-  release: ENV.CONFIG.PROJECT_VERSION,
+  release: ENV.CONFIG?.PROJECT_VERSION,
   tracesSampleRate: 1.0,
 });
