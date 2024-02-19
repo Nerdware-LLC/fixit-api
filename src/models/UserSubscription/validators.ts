a8dccd1ee0b388c109a809212d1289d303739f8f
trevor-anderson <trevor@nerdware.cloud>
2024-02-19 13:30:20 -0500

    refactor: add validator fns, rename file to suit expanded purpose

--- src/models/UserSubscription/validators.ts
+++ src/models/UserSubscription/validators.ts
@@ -1,5 +1,7 @@
+import { hasKey, isString } from "@nerdware/ts-type-safety-utils";
 import dayjs from "dayjs";
-import { hasKey } from "@/utils";
+import { pricesCache } from "@/lib/cache/pricesCache";
+import { promoCodesCache } from "@/lib/cache/promoCodesCache";
 import type { SubscriptionStatus } from "@/types";
 import type { UserSubscriptionItem } from "./UserSubscription";
 
@@ -12,14 +14,14 @@ import type { UserSubscriptionItem } from "./UserSubscription";
  *    1. The subscription's status is `active` or `trialing`.
  *    2. The subscription's `currentPeriodEnd` timestamp is in the future.
  */
-export const validateExisting = function ({
+export const validateSubscription = function ({
   status,
   currentPeriodEnd,
 }: Partial<UserSubscriptionItem> = {}) {
   if (
     !status ||
     !hasKey(SUBSCRIPTION_STATUS_METADATA, status) ||
-    SUBSCRIPTION_STATUS_METADATA?.[status]?.isValid !== true ||
+    SUBSCRIPTION_STATUS_METADATA[status]?.isValid !== true ||
     !currentPeriodEnd ||
     !dayjs(currentPeriodEnd).isValid()
   ) {
@@ -48,9 +50,11 @@ export const validateExisting = function ({
  *
  * @see https://stripe.com/docs/api/subscriptions/object#subscription_object-status
  */
-export const SUBSCRIPTION_STATUS_METADATA: Record<
-  SubscriptionStatus,
-  { isValid: true; reason?: never } | { isValid?: false; reason: string }
+export const SUBSCRIPTION_STATUS_METADATA: Readonly<
+  Record<
+    SubscriptionStatus,
+    { isValid: true; reason?: never } | { isValid?: false; reason: string }
+  >
 > = {
   // Statuses which indicate a subscription IS VALID for service usage:
   active: {
@@ -75,4 +79,39 @@ export const SUBSCRIPTION_STATUS_METADATA: Record<
   unpaid: {
     reason: "Sorry, payment for your subscription is past due. Please submit payment and try again.", // prettier-ignore
   },
-} as const;
+};
+
+/**
+ * Validates a price ID string - throws an error if the provided string is not a valid price ID.
+ * - If the provided string is a valid priceID, returns `true`.
+ * - If the provided string is not a valid priceID:
+ *   - If `shouldThrowIfInvalid` is `false` (default), returns `false`.
+ *   - If `shouldThrowIfInvalid` is `true`, throws an error.
+ */
+export const validatePriceID = (maybePriceID?: unknown, shouldThrowIfInvalid: boolean = false) => {
+  const isValid =
+    isString(maybePriceID) &&
+    pricesCache.values().some(({ id: priceID }) => maybePriceID === priceID);
+
+  if (!isValid && shouldThrowIfInvalid) throw new Error("Invalid subscription");
+
+  return isValid;
+};
+
+/**
+ * Validates a promo code string - throws an error if the provided string is not a valid promo code.
+ * - If the provided string is a valid promoCode, returns `true`.
+ * - If the provided string is not a valid promoCode:
+ *   - If `shouldThrowIfInvalid` is `false` (default), returns `false`.
+ *   - If `shouldThrowIfInvalid` is `true`, throws an error.
+ */
+export const validatePromoCode = (
+  maybePromoCode?: unknown,
+  shouldThrowIfInvalid: boolean = false
+) => {
+  const isValid = isString(maybePromoCode) && promoCodesCache.has(maybePromoCode);
+
+  if (!isValid && shouldThrowIfInvalid) throw new Error("Invalid promo code");
+
+  return isValid;
+};
