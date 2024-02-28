1828de3ab8a70d3c8b8c25cdba6bfe939ebc76e9
trevor-anderson <trevor@nerdware.cloud>
2024-02-28 12:17:17 -0500

    fix(test): add 'as any' to fake lambda fn names in test

--- src/lib/lambdaClient/lambdaClient.test.ts
+++ src/lib/lambdaClient/lambdaClient.test.ts
@@ -4,12 +4,12 @@ const MOCK_PAYLOAD = { test: "payload" };
 
 describe("lib/lambdaClient", () => {
   test("returns expected result from invokeEvent() when given valid input", async () => {
-    const result = await lambdaClient.invokeEvent("test-function", MOCK_PAYLOAD);
+    const result = await lambdaClient.invokeEvent("test-function" as any, MOCK_PAYLOAD);
     expect(result.Payload).toStrictEqual(MOCK_PAYLOAD);
   });
 
   test("returns expected result from invokeRequestResponse() when given valid input", async () => {
-    const result = await lambdaClient.invokeRequestResponse("test-function", MOCK_PAYLOAD);
+    const result = await lambdaClient.invokeRequestResponse("test-function" as any, MOCK_PAYLOAD);
     expect(result.Payload).toStrictEqual(MOCK_PAYLOAD);
   });
 });
