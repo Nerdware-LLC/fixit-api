e5e5497efb3ca0aabab7919f7e9365559023df0b
trevor-anderson <trevor@nerdware.cloud>
2024-03-24 13:04:34 -0400

    refactor: narrow req.bodu typing to only rely on 'email'

--- src/middleware/auth/findUserByEmail.ts
+++ src/middleware/auth/findUserByEmail.ts
@@ -6,7 +6,7 @@ import type { RestApiRequestBodyByPath } from "@/types/open-api.js";
  * This middleware simply queries the DB for a User with the given email address.
  */
 export const findUserByEmail = mwAsyncCatchWrapper<
-  RestApiRequestBodyByPath["/auth/register" | "/auth/login"]
+  Pick<RestApiRequestBodyByPath["/auth/register" | "/auth/login"], "email">
 >(async (req, res, next) => {
   const [user] = await User.query({
     where: { email: req.body.email },
