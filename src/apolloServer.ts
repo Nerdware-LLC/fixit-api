1c52013670ce738e5d22286f6d0d484c19948e18
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-06-22 12:21:21 -0400

    feat: add httpServer wrapper
    `app.listen` has been replaced by `httpServer.start`, which uses `node:http` under the hood, thereby facilitating the usage of Apollo's server-draining plugin on close. Instead of having Express middleware and Apollo plugins mounted/attached in their respective files, each now exports a function which runs their init logic. This is necessary to address operation ordering issues.

--- src/apolloServer.ts
+++ src/apolloServer.ts
@@ -1,43 +1,112 @@
-import { ApolloServer } from "@apollo/server";
-import { fixitSchema } from "@/graphql/schema.js";
+import { ApolloServer, type ContextFunction, type BaseContext } from "@apollo/server";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { getTypeSafeError } from "@nerdware/ts-type-safety-utils";
+import { GraphQLError } from "graphql";
+import { formatApolloError } from "@/graphql/GraphQLError/helpers.js";
+import { schema } from "@/graphql/schema.js";
 import { ENV } from "@/server/env";
-import type { FixitApiAuthTokenPayload } from "@/utils/AuthToken.js";
+import { AuthService } from "@/services/AuthService";
+import { HTTP_ERROR_CONFIGS, type HttpError } from "@/utils/httpErrors.js";
+import type { AuthTokenPayload } from "@/types/open-api.js";
 import type { Request } from "express";
+import type { Server as HttpServer } from "node:http";
 
 export const apolloServer = new ApolloServer<ApolloServerContext>({
-  schema: fixitSchema,
+  schema,
+  formatError: formatApolloError,
   csrfPrevention: true,
   introspection: ENV.IS_DEV,
   includeStacktraceInErrorResponses: !ENV.IS_PROD,
   status400ForVariableCoercionErrors: true,
-  plugins: [
-    ...(ENV.IS_DEV
-      ? [(await import("@apollo/server/plugin/inlineTrace")).ApolloServerPluginInlineTrace()]
-      : [(await import("@apollo/server/plugin/disabled")).ApolloServerPluginLandingPageDisabled()]),
-  ],
-});
+}) as ApolloServerWithContext;
 
-// Run required init logic for integrating with Express
-await apolloServer.start();
+/**
+ * `ApolloServer<ApolloServerContext>` with a custom `configurePlugins` method.
+ */
+export type ApolloServerWithContext = ApolloServer<ApolloServerContext> & {
+  /** When called, this function adds env-appropriate plugins to the ApolloServer instance. */
+  configurePlugins: (params: { httpServer: HttpServer }) => Promise<void>;
+};
+
+apolloServer.configurePlugins = async ({ httpServer }) => {
+  apolloServer.addPlugin(ApolloServerPluginDrainHttpServer({ httpServer }));
+  apolloServer.addPlugin(
+    ENV.IS_DEV
+      ? (await import("@apollo/server/plugin/inlineTrace")).ApolloServerPluginInlineTrace()
+      : (await import("@apollo/server/plugin/disabled")).ApolloServerPluginLandingPageDisabled()
+  );
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// ApolloServer Context
 
 /**
  * The `context` object available to all ApolloServer resolvers and plugins.
  */
-export type ApolloServerContext = {
-  /** The properties from the `req` object that are included in the ApolloServer `context` object. */
-  req: Pick<
-    GqlApiRequestObject,
-    "body" | "hostname" | "ip" | "ips" | "method" | "originalUrl" | "path" | "protocol" | "subdomains" // prettier-ignore
-  >;
-  /** The authenticated User (properties acquired from their AuthToken payload. */
-  user: FixitApiAuthTokenPayload<true, true>;
+export type ApolloServerContext = BaseContext & {
+  req: ApolloServerContextRequestFields;
+  user: AuthTokenPayload;
 };
 
 /**
- * The `req` object for GQL API requests.
+ * ApolloServer `context.req` — contains properties from the Express `req` object.
  */
-export type GqlApiRequestObject = Request<
-  Record<string, string>,
-  unknown,
-  { [key: string]: unknown; query?: string; operationName?: string }
+export type ApolloServerContextRequestFields = Pick<
+  Request<Record<string, string>, unknown, Record<string, unknown>>,
+  "body" | "hostname" | "ip" | "ips" | "method" | "originalUrl" | "path" | "protocol" | "subdomains"
 >;
+
+/**
+ * This function is an [ApolloServer `context` function][apollo-context-fn]
+ * which is provided to Apollo's [`expressMiddleware`][apollo-express-mw] — the
+ * entrypoint for the GraphQL API.
+ *
+ * The function validates and authenticates requests on the /api route, and ensures
+ * that no request reaches any GQL resolver unless the following are all true:
+ *
+ *   1. The request contains a valid unexpired AuthToken which was signed by the relevant key.
+ *   2. The AuthToken payload contains required User information.
+ *   3. The User's subscription is both active and unexpired.
+ *
+ * If all criteria are met, the User's information is attached to the GQL context object,
+ * along with useful properties from the request object.
+ *
+ * [apollo-express-mw]: https://www.apollographql.com/docs/apollo-server/api/express-middleware
+ * [apollo-context-fn]: https://www.apollographql.com/docs/apollo-server/data/context/#the-context-function
+ */
+export const getAuthenticatedApolloContext: ContextFunction<
+  [{ req: Request }],
+  ApolloServerContext
+> = async ({ req }): Promise<ApolloServerContext> => {
+  try {
+    // Authenticate the user
+    const authenticatedUser = await AuthService.authenticateUser.viaAuthHeaderToken(req);
+    // Ensure the User is authorized to access paid content
+    AuthService.verifyUserIsAuthorized.toAccessPaidContent({ authenticatedUser });
+    // Return the ApolloServer context object
+    return {
+      user: authenticatedUser,
+      req: {
+        body: req.body as Record<string, unknown>,
+        hostname: req.hostname,
+        ip: req.ip,
+        ips: req.ips,
+        method: req.method,
+        originalUrl: req.originalUrl,
+        path: req.path,
+        protocol: req.protocol,
+        subdomains: req.subdomains,
+      },
+    };
+  } catch (err) {
+    const error = getTypeSafeError(err);
+    // Expected error.statusCode values: 401 or 403 (default to 401 if undefined for whatever reason)
+    const errorStatusCode = (error as Partial<HttpError>).statusCode ?? 401;
+    // Get the HTTP-error-config for the statusCode
+    const httpErrorConfig = HTTP_ERROR_CONFIGS?.[errorStatusCode] ?? HTTP_ERROR_CONFIGS[401];
+    // Re-throw as GraphQLError (ApolloServer's formatError fn is not called for ctx-fn errors)
+    throw new GraphQLError(httpErrorConfig.defaultErrorMsg, {
+      extensions: httpErrorConfig.gqlErrorExtensions,
+    });
+  }
+};
--- src/expressApp.ts
+++ src/expressApp.ts
@@ -1,15 +1,13 @@
 import { expressMiddleware } from "@apollo/server/express4";
 import * as Sentry from "@sentry/node";
-import express from "express";
-import { apolloServer } from "@/apolloServer.js";
+import express, { type Express } from "express";
+import { apolloServer, getAuthenticatedApolloContext } from "@/apolloServer.js";
 import {
   corsMW,
-  errorHandler,
-  handle404,
-  logReqReceived,
-  sendRESTJsonResponse,
   setSecureHttpHeaders,
-  getValidatedGqlContext,
+  logReqReceived,
+  handle404,
+  errorHandler,
 } from "@/middleware";
 import {
   adminRouter,
@@ -17,63 +15,71 @@ import {
   connectRouter,
   subscriptionsRouter,
   webhooksRouter,
-} from "@/routers";
+} from "@/routes";
 import { ENV } from "@/server/env";
 
 /**
- * The express app for REST requests and the GraphQL entry point.
+ * The express app for API REST requests as well as the GraphQL entry point.
  *
  * > - `view cache` is always disabled since this app doesn't return HTML
  * > - `X-Powered-By` header is always disabled for security
  * > - `trust proxy` is enabled in deployed envs so the correct IP can be logged (not the LB's)
- * @see https://expressjs.com/en/4x/api.html#app.settings.table
+ *
+ * See https://expressjs.com/en/4x/api.html#app.settings.table
  */
 export const expressApp = express()
   .disable("view cache")
   .disable("x-powered-by")
-  .set("trust proxy", ENV.IS_DEPLOYED_ENV);
+  .set("trust proxy", ENV.IS_DEPLOYED_ENV) as Express & {
+  /** When called, this function mounts all middleware and route handlers for the express app. */
+  setupMiddleware: () => void;
+};
 
-// SENTRY REQUEST-HANDLER (must be first middleware)
-expressApp.use(
-  Sentry.Handlers.requestHandler({
-    // Keys to be extracted from req object and attached to the Sentry scope:
-    request: ["ip", "data", "headers", "method", "query_string", "url"],
-    ip: true,
-  })
-);
+expressApp.setupMiddleware = () => {
+  // SENTRY REQUEST-HANDLER (must be first middleware)
+  expressApp.use(
+    Sentry.Handlers.requestHandler({
+      // Keys to be extracted from req object and attached to the Sentry scope:
+      request: ["ip", "data", "headers", "method", "query_string", "url"],
+      ip: true,
+    })
+  );
 
-// LOG ALL REQUESTS
-expressApp.use(logReqReceived);
+  // LOG ALL REQUESTS
+  expressApp.use(logReqReceived);
 
-// SECURITY
-expressApp.use(corsMW, setSecureHttpHeaders);
+  // SECURITY
+  expressApp.use(corsMW, setSecureHttpHeaders);
 
-// BODY-PARSING (admin and webhooks routers handle their own body parsing)
-expressApp.use([/^\/api$/, /^\/api\/(auth|connect|subscriptions)/], express.json());
+  // BODY-PARSING (webhooks routes handle their own body parsing)
+  expressApp.use(
+    /^\/api(\/(admin|auth|connect|subscriptions))?/,
+    express.json({
+      type: ["application/json", "application/csp-report", "application/reports+json"],
+    })
+  );
 
-// REST ROUTE HANDLERS
-expressApp.use("/api/admin", adminRouter);
-expressApp.use("/api/auth", authRouter);
-expressApp.use("/api/connect", connectRouter);
-expressApp.use("/api/subscriptions", subscriptionsRouter);
-expressApp.use("/api/webhooks", webhooksRouter);
+  // REST ROUTE HANDLERS
+  expressApp.use("/api/admin", adminRouter);
+  expressApp.use("/api/auth", authRouter);
+  expressApp.use("/api/connect", connectRouter);
+  expressApp.use("/api/subscriptions", subscriptionsRouter);
+  expressApp.use("/api/webhooks", webhooksRouter);
 
-// REST RESPONSE HANDLER (admin and webhooks routers handle their own responses)
-expressApp.use(/^\/api\/(auth|connect|subscriptions)/, sendRESTJsonResponse);
+  // GRAPHQL API ENTRYPOINT (root path: /api)
+  expressApp.use(
+    "/api",
+    expressMiddleware(apolloServer, {
+      context: getAuthenticatedApolloContext,
+    })
+  );
 
-// APOLLO SERVER (root path: /api)
-expressApp.use(
-  "/api",
-  expressMiddleware(apolloServer, {
-    context: getValidatedGqlContext,
-  })
-);
+  // SENTRY ERROR-HANDLER (must be before any other error-mw and after all controllers)
+  expressApp.use(Sentry.Handlers.errorHandler());
 
-// SENTRY ERROR-HANDLER (must be before any other error-mw and after all controllers)
-expressApp.use(Sentry.Handlers.errorHandler());
+  // HANDLE NON-EXISTENT REQUEST ROUTES
+  expressApp.use(handle404);
 
-// HANDLE NON-EXISTENT REQUEST ROUTES
-expressApp.use(handle404);
-
-// UNIVERSAL FALLBACK ERROR HANDLER
-expressApp.use(errorHandler);
+  // UNIVERSAL FALLBACK ERROR HANDLER
+  expressApp.use(errorHandler);
+};
