db8efdb19055afe7523e5ecdd3d6895451d1247b
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-06-22 14:31:12 -0400

    style: extract url strings into consts

--- src/middleware/setSecureHttpHeaders.ts
+++ src/middleware/setSecureHttpHeaders.ts
@@ -3,29 +3,34 @@ import { ENV } from "@/server/env";
 import type { RequestHandler } from "express";
 
 /**
- * FIXIT CSP DIRECTIVE SOURCES
+ * This file contains Express middleware that sets HTTP headers with the intent
+ * of hardening the application's security posture, as well as approximating the
+ * headers actually returned to clients in deployed environments. In practice,
+ * live deployments operate behind various infrastructure components that override
+ * request+response headers.
  */
-const FIXIT_SOURCES = {
-  WEB: ["'self'", "https://gofixit.app", "https://*.gofixit.app"],
-  API: ["'self'", "https://gofixit.app/", "https://*.gofixit.app/"],
-} as const;
+
+// CSP DIRECTIVE SOURCES
+const SELF = "'self'";
+const API_CSP_SOURCE = `${ENV.CONFIG.API_BASE_URL}/`;
+const API_CSP_REPORT_URI = `${ENV.CONFIG.API_BASE_URL}/admin/csp-violation`;
+const GOOGLE_GSI = "https://accounts.google.com/gsi/";
+const GOOGLE_GSI_CLIENT = "https://accounts.google.com/gsi/client";
+const GOOGLE_GSI_STYLE = "https://accounts.google.com/gsi/style";
+const STRIPE_API = "https://api.stripe.com";
+const STRIPE_HOOKS = "https://hooks.stripe.com";
+const STRIPE_JS = "https://js.stripe.com";
+const SENTRY_INGEST = "https://*.ingest.sentry.io/";
 
 /**
- * Security Middleware: `setSecureHttpHeaders`
+ * This [Helmet][helmet-url] config produces a CSP that reflects a variety of sources:
  *
- * This [Helmet][helmet-url] config produces a secure `Content-Security-Policy` which serves as a
- * strong mitigation against [cross-site scripting attacks][google-xss-info]. The resultant CSP has
- * been evaluated using https://csp-evaluator.withgoogle.com/.
- *
- * These directives reflect a combination of sources:
- *
- * - HelmetJS defaults
+ * - Fixit-related sources
  * - [Stripe requirements][stripe-csp]
  * - [Google API requirements][google-api-csp]
- * - Fixit-related sources
+ * - Sentry reporting
  *
  * [helmet-url]: https://helmetjs.github.io/
- * [google-xss-info]: https://www.google.com/about/appsecurity/learning/xss/
  * [stripe-csp]: https://stripe.com/docs/security/guide#content-security-policy
  * [google-api-csp]: https://developers.google.com/identity/gsi/web/guides/get-google-api-clientid#cross_origin_opener_policy
  */
@@ -33,20 +38,20 @@ const helmetMW = helmet({
   contentSecurityPolicy: {
     useDefaults: true,
     directives: {
-      "default-src": [...FIXIT_SOURCES.WEB, "https://accounts.google.com/gsi/"],
-      "base-uri": "'self'",
-      "connect-src": [...FIXIT_SOURCES.API, "https://*.ingest.sentry.io/", "https://accounts.google.com/gsi/", "https://api.stripe.com"], // prettier-ignore
-      "font-src": ["'self'", "https:", "data:"],
-      "form-action": [...FIXIT_SOURCES.API],
-      "frame-ancestors": [...FIXIT_SOURCES.WEB],
-      "frame-src": ["https://accounts.google.com/gsi/", "https://js.stripe.com", "https://hooks.stripe.com"], // prettier-ignore
-      "img-src": [...FIXIT_SOURCES.WEB, "data:", "blob:"],
+      "default-src": [SELF, ENV.WEB_CLIENT.URL, GOOGLE_GSI],
+      "base-uri": SELF,
+      "connect-src": [SELF, API_CSP_SOURCE, SENTRY_INGEST, GOOGLE_GSI, STRIPE_API],
+      "font-src": [SELF, "https:", "data:"],
+      "form-action": [SELF, API_CSP_SOURCE],
+      "frame-ancestors": [SELF, ENV.WEB_CLIENT.URL],
+      "frame-src": [GOOGLE_GSI, STRIPE_JS, STRIPE_HOOKS],
+      "img-src": [ENV.WEB_CLIENT.URL, "data:", "blob:"],
       "object-src": "'none'",
       "report-to": "fixit-security",
-      "report-uri": `${ENV.CONFIG.API_FULL_URL}/admin/csp-violation`,
-      "script-src": [...FIXIT_SOURCES.WEB, "https://accounts.google.com/gsi/client", "https://js.stripe.com"], // prettier-ignore
+      "report-uri": API_CSP_REPORT_URI,
+      "script-src": [SELF, ENV.WEB_CLIENT.URL, GOOGLE_GSI_CLIENT, STRIPE_JS],
       "script-src-attr": "'none'",
-      "style-src": [...FIXIT_SOURCES.WEB, "https://accounts.google.com/gsi/style", "https:", "'unsafe-inline'"], // prettier-ignore
+      "style-src": [SELF, ENV.WEB_CLIENT.URL, GOOGLE_GSI_STYLE, "https:", "'unsafe-inline'"],
       "upgrade-insecure-requests": [],
     },
   },
@@ -58,7 +63,7 @@ const helmetMW = helmet({
 export const setSecureHttpHeaders: RequestHandler = (req, res, next) => {
   res.set({
     "Cache-Control": "no-store",
-    "Report-To": `{"group":"fixit-security","max_age":10886400,"url":"${ENV.CONFIG.API_FULL_URL}/admin/csp-violation"}`,
+    "Report-To": `{"group":"fixit-security","max_age":10886400,"url":"${API_CSP_REPORT_URI}"}`,
   });
 
   helmetMW(req, res, next);
