93e1356a6925cb320d8e16cce2ed99aad390e10f
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-06-22 12:23:29 -0400

    refactor: mv user-authorization logic into AuthService

--- src/services/AuthService/verifyUserIsAuthorizedToPerformThisUpdate.ts
+++ src/services/AuthService/verifyUserIsAuthorizedToPerformThisUpdate.ts
@@ -1,45 +1,40 @@
 import { hasKey } from "@nerdware/ts-type-safety-utils";
-import { GqlUserInputError, GqlForbiddenError } from "@/utils/httpErrors.js";
+import { ForbiddenError } from "@/utils/httpErrors.js";
 
 /**
  * This function performs the following common authorization validation checks for
  * update-mutations involving an item with a `status` field (e.g., `WorkOrder` and
  * `Invoice`):
  *
- * 1. Ensures the item exists
- * 2. Ensures the authenticated user is allowed to perform the update
- * 3. Ensures the item's existing `status` does not forbid the update
+ * 1. Ensures the authenticated user is allowed to perform the update
+ * 2. Ensures the item's existing `status` does not forbid the update
  */
 export function verifyUserIsAuthorizedToPerformThisUpdate<
   ItemWithStatus extends { status: string },
 >(
-  item: ItemWithStatus | undefined,
+  item: ItemWithStatus,
   {
-    itemNotFoundErrorMessage,
     idOfUserWhoCanPerformThisUpdate: allowedUserID,
     authenticatedUserID: userID,
     forbiddenStatuses,
-  }: GqlValidationParams<ItemWithStatus>
-): asserts item is NonNullable<ItemWithStatus> {
-  // Ensure the item exists
-  if (!item) {
-    throw new GqlUserInputError(itemNotFoundErrorMessage);
-  }
+  }: VeritfyUserIsAuthorizedParams<ItemWithStatus>
+) {
   // Ensure the authenticated user is allowed to perform the update
   if (allowedUserID !== userID) {
-    throw new GqlForbiddenError("Access denied.");
+    throw new ForbiddenError("Access denied.");
   }
   // Ensure the item's existing `status` does not forbid the update
   if (forbiddenStatuses && hasKey(forbiddenStatuses, item.status)) {
-    throw new GqlForbiddenError(forbiddenStatuses[item.status as keyof typeof forbiddenStatuses]);
+    throw new ForbiddenError(forbiddenStatuses[item.status as keyof typeof forbiddenStatuses]);
   }
 }
 
 /**
  * Validation parameters for controlling the behavior of {@link verifyUserCanPerformThisUpdate}.
  */
-export type GqlValidationParams<ItemWithStatus extends { status: string } = { status: string }> = {
-  itemNotFoundErrorMessage: string;
+export type VeritfyUserIsAuthorizedParams<
+  ItemWithStatus extends { status: string } = { status: string },
+> = {
   idOfUserWhoCanPerformThisUpdate: string | null | undefined;
   authenticatedUserID: string;
   forbiddenStatuses?: ForbiddenStatusErrorMessages<ItemWithStatus>;
