fc4eba4416df35ac5f52af2ea21ad767ac15024c
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-06-22 12:09:35 -0400

    fix(mock): promisify return type of SendMessagesCommand

--- __mocks__/@aws-sdk/client-pinpoint.ts
+++ __mocks__/@aws-sdk/client-pinpoint.ts
@@ -1,14 +1,14 @@
 import { mockClient } from "aws-sdk-client-mock";
 
-const { PinpointClient: _PinpointClient, SendMessagesCommand } = await vi.importActual<
+const { PinpointClient: Actual_PinpointClient, SendMessagesCommand } = await vi.importActual<
   typeof import("@aws-sdk/client-pinpoint")
 >("@aws-sdk/client-pinpoint");
 
 const PinpointClient = vi.fn(
   () =>
-    mockClient(_PinpointClient)
+    mockClient(Actual_PinpointClient)
       .on(SendMessagesCommand)
-      .callsFake(() => ({})) // return an empty object
+      .callsFake(() => Promise.resolve({})) // return an empty object
 );
 
 export { PinpointClient, SendMessagesCommand };
