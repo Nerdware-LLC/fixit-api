9d16b6e81c3a78ecb7700338e8f383134ab6ee74
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-06-24 08:01:19 -0400

    refactor: simplify Sentry init logic

--- src/server/initSentry.ts
+++ src/server/initSentry.ts
@@ -1,12 +1,16 @@
 import * as Sentry from "@sentry/node";
 import { ENV } from "@/server/env";
 
-if (/^(dev|staging|prod)/i.test(ENV.NODE_ENV) && !!ENV?.SENTRY_DSN) {
+if (ENV.SENTRY_DSN) {
   Sentry.init({
     enabled: true,
     dsn: ENV.SENTRY_DSN,
     environment: ENV.NODE_ENV,
     tracesSampleRate: 1.0,
-    ...(!!ENV.CONFIG?.PROJECT_VERSION && { release: ENV.CONFIG.PROJECT_VERSION }),
+    ...(ENV.CONFIG?.PROJECT_VERSION && {
+      release: ENV.CONFIG.PROJECT_VERSION,
+    }),
   });
+} else if (ENV.IS_DEPLOYED_ENV) {
+  throw new Error("Unable to initialize Sentry in deployed env: Missing SENTRY_DSN");
 }
