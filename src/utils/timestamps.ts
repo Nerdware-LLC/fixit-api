040ae6eae40d6d1049739b97cd6910de276e607e
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-06-24 07:07:56 -0400

    refactor: mv unix-timestamp regex here

--- src/utils/timestamps.ts
+++ src/utils/timestamps.ts
@@ -64,3 +64,13 @@ export const isValidTimestamp = (
   // `value` is cast to any, bc the previous checks ensure it won't throw
   return dayjs(value as any).isValid();
 };
+
+/**
+ * Regex pattern for validating Unix timestamps of either 9 or 10 digits. The first
+ * digit must be a non-zero integer. The 9-10 digit restriction results in the date
+ * range shown below, which suits the needs of this application better than a more
+ * generalized Unix timestamp regex pattern.
+ * - Min: `100000000`  = `Saturday, Mar  3, 1973  9:46:40 am UTC`
+ * - Max: `9999999999` = `Saturday, Nov 20, 2286 17:46:39 pm UTC`
+ */
+export const UNIX_TIMESTAMP_REGEX = /^[1-9]\d{8,9}$/;
