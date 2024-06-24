39e31762d543e83c411383e071745537f729170c
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-06-24 07:07:12 -0400

    refactor: improve typing for ErrorLoggerFn 'error' arg

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -8,7 +8,7 @@ import * as Sentry from "@sentry/node";
 import chalk, { type ChalkInstance } from "chalk";
 import dayjs from "dayjs";
 import { ENV } from "@/server/env";
-import type { HttpErrorInterface } from "./httpErrors";
+import type { ErrorOrHttpError } from "@/types/globals.js";
 
 /* eslint-disable no-console */
 
@@ -206,6 +206,6 @@ type LoggerFn = (
 
 /** Internal type for `handleLogError` fns used in `getLoggerUtil`. */
 type ErrorLoggerFn = (
-  error: Error & Partial<HttpErrorInterface>,
+  error: ErrorOrHttpError,
   msgPrefix?: GetLogMessageArgsProvidedByHandler["msgPrefix"]
 ) => void;
