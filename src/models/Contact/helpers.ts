c9486608de0988fcba1f1c53c53ac085eae07c5e
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-06-28 10:58:11 -0400

    refactor: replace sanitizeID w userModelHelpers.id.sanitize

--- src/models/Contact/helpers.ts
+++ src/models/Contact/helpers.ts
@@ -1,4 +1,3 @@
-import { sanitizeID } from "@nerdware/ts-string-helpers";
 import { userModelHelpers } from "@/models/User/helpers.js";
 import { createMapOfStringAttrHelpers, getCompoundAttrRegex, DELIMETER } from "@/models/_common";
 
@@ -9,7 +8,7 @@ export const contactModelHelpers = createMapOfStringAttrHelpers({
     /** Validation regex for Contact IDs. */
     regex: getCompoundAttrRegex([CONTACT_SK_PREFIX_STR, userModelHelpers.id.regex]),
     /** Sanitizes a Contact ID value. */
-    sanitize: sanitizeID,
+    sanitize: userModelHelpers.id.sanitize,
     /** Returns a formatted Contact "id" value. */
     format: (contactUserID: string) => `${CONTACT_SK_PREFIX_STR}${DELIMETER}${contactUserID}`,
   },
