f9145b427ca11c80c8684f800ec7dd97030c0271
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-06-28 10:58:43 -0400

    fix: replace sanitizeID w correct regex impl

--- src/models/Invoice/helpers.ts
+++ src/models/Invoice/helpers.ts
@@ -1,25 +1,21 @@
-import { sanitizeID } from "@nerdware/ts-string-helpers";
 import { isSafeInteger } from "@nerdware/ts-type-safety-utils";
 import { userModelHelpers } from "@/models/User/helpers.js";
-import { createMapOfStringAttrHelpers, getCompoundAttrRegex, DELIMETER } from "@/models/_common";
+import { createHelpersForStrAttr, getCompoundAttrRegex, DELIMETER } from "@/models/_common";
 import { getRandomUUIDv4, UUID_REGEX } from "@/utils/uuid.js";
 
 export const INVOICE_SK_PREFIX_STR = "INV";
 
 export const invoiceModelHelpers = {
-  ...createMapOfStringAttrHelpers({
-    id: {
-      /** Invoice ID validation regex. */
-      regex: getCompoundAttrRegex([INVOICE_SK_PREFIX_STR, userModelHelpers.id.regex, UUID_REGEX]),
-      /** Sanitizes an Invoice ID value. */
-      sanitize: sanitizeID,
-      /** Invoice "id" value formatter. */
-      format: (createdByUserID: string) => {
-        return `${INVOICE_SK_PREFIX_STR}${DELIMETER}${createdByUserID}${DELIMETER}${getRandomUUIDv4()}`;
-      },
+  id: createHelpersForStrAttr("id", {
+    /** Invoice ID validation regex. */
+    regex: getCompoundAttrRegex([INVOICE_SK_PREFIX_STR, userModelHelpers.id.regex, UUID_REGEX]),
+    /** Sanitizes an Invoice ID value. */
+    sanitize: (str: string) => str.replace(/[^a-zA-Z0-9_@#-]/g, ""), // handle chars, UUID chars, and the delimeter
+    /** Invoice "id" value formatter. */
+    format: (createdByUserID: string) => {
+      return `${INVOICE_SK_PREFIX_STR}${DELIMETER}${createdByUserID}${DELIMETER}${getRandomUUIDv4()}`;
     },
   }),
-  /** `Invoice.amount` attribute helpers */
   amount: {
     isValid: (value?: unknown) => isSafeInteger(value) && value > 0,
   },
